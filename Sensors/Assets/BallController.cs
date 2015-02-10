70458ad2e68db7ce890477dd44a295bcd5542388
RAM <RAM@asus.(none)>
2015-02-10 16:37:10 -0500

    Fixed_movement

--- Sensors/Assets/BallController.cs
+++ Sensors/Assets/BallController.cs
@@ -6,11 +6,13 @@ public class BallController : MonoBehaviour {
 	public float speed = 10.0F;
 	public float rotationSpeed = 100.0F;
 	void Update() {
+
 		float translation = Input.GetAxis("Vertical") * speed;
 		float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
 		translation *= Time.deltaTime;
 		rotation *= Time.deltaTime;
-		transform.Translate(0, translation, 0);
-		transform.Rotate(rotation, rotation, 0);
+
+		transform.Translate(translation, 0, 0);
+		transform.Rotate(0, 0, rotation);
 	}
 }
