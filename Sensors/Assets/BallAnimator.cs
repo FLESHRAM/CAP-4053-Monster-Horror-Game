cc320f63fb2fb506cfee2de2589884fd03fd8c85
RAM <RAM@asus.(none)>
2015-02-09 23:32:45 -0500

    Sensors

---
+++ Sensors/Assets/BallAnimator.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallAnimator : MonoBehaviour {
+
+
+	public Sprite[] sprites;
+	public float FPS;
+	private SpriteRenderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = renderer as SpriteRenderer;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		int index = (int)(Time.timeSinceLevelLoad * FPS);
+		index = index % sprites.Length;
+		rend.sprite = sprites[ index ];
+	}
+}
---
+++ Sensors/Assets/BallController.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+
+	public float speed = 10.0F;
+	public float rotationSpeed = 100.0F;
+	void Update() {
+		float translation = Input.GetAxis("Vertical") * speed;
+		float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+		translation *= Time.deltaTime;
+		rotation *= Time.deltaTime;
+		transform.Translate(0, translation, 0);
+		transform.Rotate(rotation, rotation, 0);
+	}
+}
