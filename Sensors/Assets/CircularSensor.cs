82527c9c2fb7ed58fe71a3e51ceaa7f3ba2b5480
DarthEnchilada <beiercm@gmail.com>
2015-02-11 02:27:54 -0500

    CircularSensor
    Created circular sensor

---
+++ Sensors/Assets/CircularSensor.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CircularSensor : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		mousePos.z = 0;
+		transform.position = mousePos;	
+	}
+
+
+}
