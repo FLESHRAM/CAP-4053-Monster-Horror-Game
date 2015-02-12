1b1c76e1dfb2858401729c0294c129fa4c93c0c5
lsosa <sosaberriz@knights.ucf.edu>
2015-02-11 23:17:42 -0500

    Adding some code to test object sensing

--- Sensors/Assets/BallController.cs
+++ Sensors/Assets/BallController.cs
@@ -5,6 +5,8 @@ public class BallController : MonoBehaviour {
 
 	public float moveSpeed = 8.0F;
 	public float rotSpeed = 200.0F;
+	public Vector3 stuff;
+
 	void Update() {
 
 		float move = Input.GetAxis("Vertical") * moveSpeed;
@@ -13,6 +15,13 @@ public class BallController : MonoBehaviour {
 		rot *= Time.deltaTime;
 
 		transform.Translate(move, 0, 0);
-		transform.Rotate(0, 0, rot);
+		transform.Rotate(0, 0, -rot);
+
+		Collider[] colliders = Physics.OverlapSphere (transform.position, 20f);
+		foreach (Collider col in colliders) {
+			Debug.Log ("I hit: " + col.name);
+			stuff = col.transform.position;
+			Debug.DrawLine(transform.position, stuff, Color.red);
+		}
 	}
 }
