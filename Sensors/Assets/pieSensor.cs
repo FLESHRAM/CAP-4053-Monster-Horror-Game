fcf67cfab3e2c55aff04899e819434f20f94a296
lsosa <sosaberriz@knights.ucf.edu>
2015-02-15 15:28:08 -0500

    Added guide lines

--- Sensors/Assets/pieSensor.cs
+++ Sensors/Assets/pieSensor.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class pieSensor : MonoBehaviour {
 
+	public Transform fStart_line1, fEnd_line1;
+	public Transform fStart_line2, fEnd_line2;
+
 	// Use this for initialization
 	void Start () {
 	
@@ -10,6 +13,8 @@ public class pieSensor : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+
+		Debug.DrawLine (fStart_line1.position, fEnd_line1.position, Color.white);
+		Debug.DrawLine (fStart_line2.position, fEnd_line2.position, Color.white);
 	}
 }
