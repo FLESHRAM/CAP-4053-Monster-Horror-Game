efcf72f4aa967206252a929b3f4976ea3fc3fa1b
lsosa <sosaberriz@knights.ucf.edu>
2015-02-15 14:56:10 -0500

    Adding a script for the pie sensor

--- Sensors/Assets/BallController.cs
+++ Sensors/Assets/BallController.cs
@@ -5,7 +5,6 @@ public class BallController : MonoBehaviour {
 
 	public float moveSpeed = 8.0F;
 	public float rotSpeed = 200.0F;
-	public Vector3 stuff;
 
 	void Update() {
 
@@ -16,12 +15,5 @@ public class BallController : MonoBehaviour {
 
 		transform.Translate(move, 0, 0);
 		transform.Rotate(0, 0, -rot);
-
-		Collider[] colliders = Physics.OverlapSphere (transform.position, 20f);
-		foreach (Collider col in colliders) {
-			Debug.Log ("I hit: " + col.name);
-			stuff = col.transform.position;
-			Debug.DrawLine(transform.position, stuff, Color.red);
-		}
 	}
 }
