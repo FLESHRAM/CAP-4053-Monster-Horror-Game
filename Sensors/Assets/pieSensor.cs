552fbc602b901a0ce06a16d3795010c7b347e378
lsosa <sosaberriz@knights.ucf.edu>
2015-02-15 18:21:20 -0500

    Updating the pie sensor

--- Sensors/Assets/pieSensor.cs
+++ Sensors/Assets/pieSensor.cs
@@ -16,5 +16,12 @@ public class pieSensor : MonoBehaviour {
 
 		Debug.DrawLine (fStart_line1.position, fEnd_line1.position, Color.white);
 		Debug.DrawLine (fStart_line2.position, fEnd_line2.position, Color.white);
+
+		Collider2D[] colliders = Physics2D.OverlapCircleAll (transform.position, 1.75f);
+
+		foreach (Collider2D col in colliders) {
+
+			Debug.Log("I see: " + col.name + " At an agle: " + Vector2.Angle(transform.position,col.transform.position));
+		}
 	}
 }
