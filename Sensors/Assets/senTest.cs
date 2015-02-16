b6860109bb3b270a74b26c1284a66a3314d249c3
U-RAM-TOWER\RAM <RAM@RAM-TOWER.(none)>
2015-02-15 19:17:10 -0500

    more_senTest

--- Sensors/Assets/senTest.cs
+++ Sensors/Assets/senTest.cs
@@ -4,6 +4,10 @@ using System.Collections;
 public class senTest : MonoBehaviour {
 
 	public Transform fStart, fEnd;
+	//public Object[] FLoaters;
+	public float radarRadius = 2f; 
+	private RaycastHit2D prevHit;
+	private Collider2D[] prevFloaters;
 
 	// Use this for initialization
 	void Start () {
@@ -13,17 +17,47 @@ public class senTest : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		Raycasting ();
+		Radar();
 	}
 
 
 
 	void Raycasting() {
 
+		if(prevHit.collider != null) prevHit.rigidbody.gameObject.renderer.material.color = Color.white;
 		RaycastHit2D hit = Physics2D.Raycast (fStart.position, fEnd.position, Vector2.Distance (fStart.position, fEnd.position), 1 << LayerMask.NameToLayer("Obstacle"));
 
 		if (hit.collider != null) {
 			Debug.DrawLine(fStart.position, hit.point, Color.green);		
-		
+			hit.rigidbody.gameObject.renderer.material.color = Color.green;
+			prevHit = hit;
+		}
+	}
+
+
+	void Radar()
+	{
+		if (prevFloaters != null) 
+		{
+			for(int i = 0; i<prevFloaters.Length; i++)
+			{
+				prevFloaters[i].attachedRigidbody.gameObject.renderer.material.color = Color.white;
+			}
+
+			System.Array.Clear(prevFloaters, 0, prevFloaters.Length);
+		}
+
+		Collider2D[] currFloaters = Physics2D.OverlapCircleAll (transform.position, radarRadius, 1 << LayerMask.NameToLayer ("Floater"));
+
+		if (currFloaters != null) 
+		{
+			for(int i = 0; i<currFloaters.Length; i++)
+			{
+				currFloaters[i].attachedRigidbody.gameObject.renderer.material.color = Color.red;
+			}
+
+			prevFloaters = (UnityEngine.Collider2D[])currFloaters.Clone();
+			System.Array.Clear(currFloaters, 0, currFloaters.Length);
 		}
 	}
 }
