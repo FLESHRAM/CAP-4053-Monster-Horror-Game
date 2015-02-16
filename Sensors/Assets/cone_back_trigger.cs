c2bef72613d8d4aad58f4f30916dd2580a702cba
lsosa <sosaberriz@knights.ucf.edu>
2015-02-15 23:48:02 -0500

    Updated pie sensor
    Added the other empty objects to detect left, back, and right objects.

--- Sensors/Assets/cone_back_trigger.cs
+++ Sensors/Assets/cone_back_trigger.cs
@@ -1,16 +1,16 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class cone_front_trigger : MonoBehaviour {
+public class cone_back_trigger : MonoBehaviour {
 
-	public int isInfront = 0;
-
+	public int isOnBack = 0;
+	
 	void OnTriggerEnter2D(Collider2D other){
 		//Debug.Log ("Object Entered the trigger");
-		isInfront = 1;
+		isOnBack = 1;
 	}
 	void OnTriggerExit2D(Collider2D other) {
 		//Debug.Log ("Object Exited the trigger");
-		isInfront = 0;
+		isOnBack = 0;
 	}
 }
--- Sensors/Assets/pieSensor.cs
+++ Sensors/Assets/pieSensor.cs
@@ -5,13 +5,20 @@ public class pieSensor : MonoBehaviour {
 
 	public Transform fStart_line1, fEnd_line1;
 	public Transform fStart_line2, fEnd_line2;
+
 	private cone_front_trigger cone_front;
+	private cone_left_trigger cone_left;
+	private cone_back_trigger cone_back;
+	private cone_right_trigger cone_right;
 
 	// Use this for initialization
 	void Start () {
 		//GameObject frontCone = GameObject.Find ("cone_front");
 		//cone_front_trigger cone_front = frontCone.GetComponent<cone_front_trigger> ();
 		cone_front = GameObject.Find ("cone_front").GetComponent<cone_front_trigger> ();
+		cone_left = GameObject.Find ("cone_left").GetComponent<cone_left_trigger> ();
+		cone_back = GameObject.Find ("cone_back").GetComponent<cone_back_trigger> ();
+		cone_right = GameObject.Find ("cone_right").GetComponent<cone_right_trigger> ();
 	}
 	
 	// Update is called once per frame
@@ -19,11 +26,8 @@ public class pieSensor : MonoBehaviour {
 
 		//Debug.DrawLine (fStart_line1.position, fEnd_line1.position, Color.white);
 		//Debug.DrawLine (fStart_line2.position, fEnd_line2.position, Color.white);
-		if (cone_front.isInfront) {
-			Debug.Log ("Detected something in front!");
-		} else {
-			Debug.Log ("");
-		}
+		Debug.Log ("Return: " + cone_front.isInfront.ToString() + "," + cone_left.isOnLeft.ToString() + "," + cone_back.isOnBack.ToString() + "," + cone_right.isOnRight.ToString());
+
 
 		Collider2D[] colliders = Physics2D.OverlapCircleAll (transform.position, 1.75f);
 
