f0274924e03dc83d0007a5f4573b43000d5f2df8
lsosa <sosaberriz@knights.ucf.edu>
2015-02-16 16:48:44 -0500

    updating pie sensor

--- Sensors/Assets/pieSensor.cs
+++ Sensors/Assets/pieSensor.cs
@@ -13,8 +13,6 @@ public class pieSensor : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		//GameObject frontCone = GameObject.Find ("cone_front");
-		//cone_front_trigger cone_front = frontCone.GetComponent<cone_front_trigger> ();
 		cone_front = GameObject.Find ("cone_front").GetComponent<cone_front_trigger> ();
 		cone_left = GameObject.Find ("cone_left").GetComponent<cone_left_trigger> ();
 		cone_back = GameObject.Find ("cone_back").GetComponent<cone_back_trigger> ();
@@ -27,13 +25,5 @@ public class pieSensor : MonoBehaviour {
 		Debug.DrawLine (fStart_line1.position, fEnd_line1.position, Color.white);
 		Debug.DrawLine (fStart_line2.position, fEnd_line2.position, Color.white);
 		Debug.Log ("Return: " + cone_front.isInfront.ToString() + "," + cone_left.isOnLeft.ToString() + "," + cone_back.isOnBack.ToString() + "," + cone_right.isOnRight.ToString());
-
-
-		Collider2D[] colliders = Physics2D.OverlapCircleAll (transform.position, 1.75f);
-
-		foreach (Collider2D col in colliders) {
-
-			//Debug.Log("I see: " + col.name + " At an agle: " + Vector2.Angle(fStart_line1.position,col.transform.position));
-		}
 	}
 }
