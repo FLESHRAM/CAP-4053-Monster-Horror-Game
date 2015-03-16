a449539ce9d2c88447e833bcb00d72a607808a1c
RAM <RAM@asus.(none)>
2015-03-16 16:15:43 -0400

    path

---
+++ Sensors/Assets/testBrain.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class testBrain : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 currPos = transform.position;
+	}
+}
