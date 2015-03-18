28391ad83bb345d79725c938a50a8c5574fcd0e0
U-RAM-TOWER\RAM <RAM@RAM-TOWER.(none)>
2015-03-18 00:19:58 -0400

    starting_setup

--- Sensors/Assets/Scripts/testBrain.cs
+++ Sensors/Assets/Scripts/testBrain.cs
@@ -12,7 +12,6 @@ public class testBrain : MonoBehaviour {
 	private NodeData data;
 
 	public bool stop;
-	private bool newMovement;
 
 	// Use this for initialization
 	void Start () {
@@ -21,7 +20,6 @@ public class testBrain : MonoBehaviour {
 		data = startNode.GetComponent("NodeData") as NodeData;
 
 		targetPos = data.down.transform.position;
-		newMovement = true;
 
 		data.debugLines (data.name);
 	}
@@ -31,28 +29,26 @@ public class testBrain : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
-		if(transform.position == targetPos) stop = true;
+		if(Vector3.Distance(transform.position, targetPos) < 0.003f ) stop = true;
 
 		if (!stop) {
 			Vector3 currPos = transform.position;
 			
-			if (newMovement) {
+
 				moveDir = targetPos - currPos;
 				moveDir.z = 0;
 				moveDir.Normalize ();
 				
 				
-				newMovement = false;
-			}
 
-			if(transform.position == targetPos) stop = true;
+
 			Vector3 target = moveDir * 1f + currPos;
 			transform.position = Vector3.Lerp (currPos, target, Time.deltaTime);
-			if(transform.position == targetPos) stop = true;
+
 			float angle = Mathf.Atan2 (moveDir.y, moveDir.x) * Mathf.Rad2Deg;
 			transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.Euler (0, 0, angle), 1f * Time.deltaTime);
-			if(transform.position == targetPos) stop = true;
-			newMovement = true;
+
+			if(Vector3.Distance(transform.position, targetPos) < 0.003f ) stop = true;
 		}
 
 		//else if (stop) { transform.position = targetPos; }
