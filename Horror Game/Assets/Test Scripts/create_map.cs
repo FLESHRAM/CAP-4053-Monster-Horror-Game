c3b3f2c1e763b3dee34c5693a57448e9bee80ebe
lsosa <sosaberriz@knights.ucf.edu>
2015-03-25 22:39:37 -0400

    Map Creation
    Added more prefab and wrote some code to create a simple boundary

--- Horror Game/Assets/Test Scripts/create_map.cs
+++ Horror Game/Assets/Test Scripts/create_map.cs
@@ -13,11 +13,45 @@ public class create_map : MonoBehaviour {
 	public Transform floor_2;
 	public Transform floor_1;
 
+	public Transform small_vertical_wall;
+	public Transform wall_vertical;
+	public Transform wall_horizontal;
+
+	public Transform top_left_corner;
+	public Transform top_right_corner;
+	public Transform bottom_right_corner;
+	public Transform bottom_left_corner;
+
 	// Use this for initialization
 	void Start () {
-		for (float x = -9f; x < 10f; x += 0.6f) {
-			Instantiate (floor_1, new Vector3 (x, 5.0f, 0.0f), Quaternion.identity);
+
+		Instantiate (top_left_corner, new Vector3 (-9.64f, 5.64f, 0.0f), Quaternion.identity);
+
+		for (float x = -9f; x < 10f; x += 0.64f) {
+			Instantiate (wall_horizontal, new Vector3 (x, 5.64f, 0.0f), Quaternion.identity);
 		}
+
+		Instantiate (top_right_corner, new Vector3 (-9.64f, 5.64f, 0.0f), Quaternion.identity);
+
+		for (float y = 5.64f; y > -10f; y -= 0.64f) {
+			Instantiate (wall_vertical, new Vector3 (-9.64f, y, 0.0f), Quaternion.identity);
+		}
+
+		Instantiate (bottom_left_corner, new Vector3 (-9.64f, -10.36f, 0.0f), Quaternion.identity);
+
+		for (float x = -9f; x < 10f; x += 0.64f) {
+			Instantiate (wall_horizontal, new Vector3 (x, -10.36f, 0.0f), Quaternion.identity);
+		}
+
+		Instantiate (bottom_right_corner, new Vector3 (10.2f, -10.36f, 0.0f), Quaternion.identity);
+
+		for (float y = -10.36f; y < 5.0f; y += 0.64f) {
+			Instantiate (wall_vertical, new Vector3 (10.2f, y, 0.0f), Quaternion.identity);
+		}
+
+		Instantiate (top_right_corner, new Vector3 (10.2f, 5.64f, 0.0f), Quaternion.identity);
+
+
 	}
 	
 	// Update is called once per frame
