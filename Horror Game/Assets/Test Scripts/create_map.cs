e0442354b14db454e9e80352f7a7630ef0dd9cfc
DarthEnchilada <beiercm@gmail.com>
2015-03-30 02:57:00 -0400

    Organized the code in create_map

--- Horror Game/Assets/Test Scripts/create_map.cs
+++ Horror Game/Assets/Test Scripts/create_map.cs
@@ -24,58 +24,82 @@ public class create_map : MonoBehaviour {
 
 	private Transform[] floor_patterns;
 
+	private int wallResources = 100;
+	private float leftBound = -16;
+	private float rightBound = 16;
+	private float upperBound = 8;
+	private float lowerBound = -8;
+	private float increment = .64f;
+
+
 	// Use this for initialization
 	void Start () {
 
-		floor_patterns = new Transform[6] {floor_2,floor_3,floor_5,floor_6,floor_7,floor_8};
-
 		//Create the outside square
-		Instantiate (top_left_corner, new Vector3 (-16.0f, 8.0f, 0.0f), Quaternion.identity);
+		createWalls();
 
-		for (float x = -15.36f; x < 15.64f; x += 0.64f) {
-			Instantiate (wall_horizontal, new Vector3 (x, 8.0f, 0.0f), Quaternion.identity);
-		}
+		createFloor();
 
-		Instantiate (top_right_corner, new Vector3 (16.0f, 8.0f, 0.0f), Quaternion.identity);
+		spawnObstacles();
 
-		for (float y = 8.0f; y > -7.64f; y -= 0.64f) {
-			Instantiate (wall_vertical, new Vector3 (-16.0f, y, 0.0f), Quaternion.identity);
-		}
+				/*
 
-		Instantiate (bottom_left_corner, new Vector3 (-16.0f, -8.0f, 0.0f), Quaternion.identity);
+				if(x < -11 && y > 3)
+					rand = 0;
 
-		for (float x = -15.36f; x < 15.64f; x += 0.64f) {
-			Instantiate (wall_horizontal, new Vector3 (x, -8.0f, 0.0f), Quaternion.identity);
-		}
-
-		Instantiate (bottom_right_corner, new Vector3 (16.0f, -8.0f, 0.0f), Quaternion.identity);
-
-		for (float y = -7.36f; y < 7.64f; y += 0.64f) {
-			Instantiate (wall_vertical, new Vector3 (16.0f, y, 0.0f), Quaternion.identity);
-		}
-
-
-		//Create the floor patter
-		for (float y = 7.36f; y > -7.64f; y -= 0.64f) {
-			for (float x = -15.36f; x < 15.64f; x += 0.64f) {
-
-				int rand = Random.Range(0, 10);
-
-				if ((x != -15.36f || y != 7.36f) && rand == 4) {
+				if ((x != -15.36f && y != 7.36f) && rand == 4) {
 					Instantiate (wall_vertical, new Vector3 (x, y, 0.0f), Quaternion.identity);
 				}
-				else if ((x != -15.36f || y != 7.36f) && rand == 8) {
+				else if ((x != -15.36f && y != 7.36f) && rand == 8) {
 					Instantiate (wall_horizontal, new Vector3 (x, y, 0.0f), Quaternion.identity);
 				}
 				else {
 					Instantiate (floor_patterns [Random.Range (0, 6)], new Vector3 (x, y, 0.0f), Quaternion.identity);
 				}
-			}
+				*/
+	
+	}
+
+	void createWalls()
+	{
+		Instantiate (top_left_corner, new Vector3 (leftBound, upperBound, 0.0f), Quaternion.identity);
+		Instantiate (top_right_corner, new Vector3 (16.0f, 8.0f, 0.0f), Quaternion.identity);
+		Instantiate (bottom_left_corner, new Vector3 (-16.0f, -8.0f, 0.0f), Quaternion.identity);
+		Instantiate (bottom_right_corner, new Vector3 (16.0f, -8.0f, 0.0f), Quaternion.identity);
+
+		for (float x = leftBound + increment; x < 15.64f; x += increment) {
+			Instantiate (wall_horizontal, new Vector3 (x, upperBound, 0.0f), Quaternion.identity);
+			Instantiate (wall_horizontal, new Vector3 (x, lowerBound, 0.0f), Quaternion.identity);
 		}
 
+		for (float y = upperBound; y > -7.64f; y -= increment) {
+			Instantiate (wall_vertical, new Vector3 (leftBound, y, 0.0f), Quaternion.identity);
+			Instantiate (wall_vertical, new Vector3 (rightBound, y, 0.0f), Quaternion.identity);
+		}
 
 	}
-	
+
+	void createFloor()
+	{
+		floor_patterns = new Transform[6] {floor_2,floor_3,floor_5,floor_6,floor_7,floor_8};
+
+		for (float y = 7.36f; y > -7.64f; y -= 0.64f)
+			for (float x = -15.36f; x < 15.64f; x += 0.64f)
+				Instantiate (floor_patterns [Random.Range (0, 6)], new Vector3 (x, y, 0.0f), Quaternion.identity);
+	}
+
+	void spawnObstacles()
+	{
+		for (float y = 7.36f; y > -7.64f; y -= 0.64f)
+			for (float x = -15.36f; x < 15.64f; x += 0.64f)
+				if(Random.Range(0,1) == 1 && wallResources > 0)
+				{
+					Instantiate (wall_vertical, new Vector3 (x, y, 1.0f), Quaternion.identity);
+					wallResources--;
+				}
+
+	}
+
 	// Update is called once per frame
 	void Update () {
 	
