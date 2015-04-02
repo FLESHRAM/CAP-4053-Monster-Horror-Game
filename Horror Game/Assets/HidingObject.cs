24dd62cfede72c91276e7f666631013a49ba8f44
RAM <RAM@asus.(none)>
2015-04-02 13:30:14 -0400

    more_testing

---
+++ Horror Game/Assets/HidingObject.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HidingObject : MonoBehaviour {
+
+
+	public GameObject node;
+
+	// Use this for initialization
+	void Start () {
+		node = null;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(node == null)
+		{
+			Collider2D hit = Physics2D.OverlapCircle(transform.position, 0.36f);
+			node = hit.gameObject;
+		}
+	}
+}
