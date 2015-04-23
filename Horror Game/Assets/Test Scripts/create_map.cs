b2338de88029eef06669958d264ae1bc7a544ac0
DarthEnchilada <beiercm@gmail.com>
2015-04-23 15:26:46 -0400

    Fixed the maps coming out too small

--- Horror Game/Assets/Test Scripts/create_map.cs
+++ Horror Game/Assets/Test Scripts/create_map.cs
@@ -34,12 +34,16 @@ public class create_map : MonoBehaviour {
 
 	private List<GameObject> wallSections = new List<GameObject>();
 	
-	int xsize = 50;
-	int ysize = 24;
+	int mazeSize = 0; 
+	int xsize;
+	int ysize;
 	char [,,] maze;
 	
 	// Use this for initialization
 	void Start () {
+		
+		xsize = Random.Range(40, 101);
+		ysize = xsize / 2;
 		
 		createMaze();
 		
@@ -49,29 +53,38 @@ public class create_map : MonoBehaviour {
 	void createMaze()
 	{
 		floor_patterns = new Transform[6] {floor_2,floor_3,floor_5,floor_6,floor_7,floor_8};
-		maze = new char[ysize, xsize, 2];
 		
-		for(int i = 0; i < ysize; i++)
-			for(int j = 0; j < xsize; j++)
+		
+		
+		{
+			mazeSize = 0;
+			maze = new char[ysize, xsize, 2];
+			for(int i = 0; i < ysize; i++)
+				for(int j = 0; j < xsize; j++)
 			{
 				maze[i,j, 0] = '_';
 				maze[i,j, 1] = 'u';
 			}	
 			
-		List<Coordinate> coordStack = new List<Coordinate>();
-		coordStack.Add(new Coordinate(0,0));
-		coordStack.Add(new Coordinate(0, 1));
-		coordStack.Add (new Coordinate(1, 0));
-		fillInMaze(coordStack);
+			List<Coordinate> coordStack = new List<Coordinate>();
+			coordStack.Add(new Coordinate(0,0));
+			coordStack.Add(new Coordinate(0, 1));
+			coordStack.Add (new Coordinate(1, 0));
+			fillInMaze(coordStack);
+		}
 
 		for(int i = 0; i < ysize; i++)
 		{				
 			for(int j = 0; j < xsize; j++)
 			{
+				float x = j * increment - ( increment * xsize / 2);
+				float y = (increment * ysize / 2) - (i * increment);
+			
 				if(maze[i,j, 0] == 'f')
-					Instantiate (floor_patterns [Random.Range (0, 6)], new Vector3 (j * increment + (leftBound + increment), (upperBound - increment) - (i * increment), 1.0f), Quaternion.identity);
+					Instantiate (floor_patterns [Random.Range (0, 6)], new Vector3 (x, y, 1.0f), Quaternion.identity);
 				else
-					Instantiate (wall_vertical, new Vector3 (j * increment + (leftBound + increment), (upperBound - increment) - (i * increment), 0.0f), Quaternion.identity);
+				//else if(maze[i,j,0] == 'w') //Leaves black spaces where no walls or floors are
+					Instantiate (wall_vertical, new Vector3 (x, y, 0.0f), Quaternion.identity);
 			}
 		}
 				
@@ -79,6 +92,93 @@ public class create_map : MonoBehaviour {
 	}
 	
 	void fillInMaze(List<Coordinate> coordStack)
+	{
+		if(coordStack.Count < 1)
+			return;
+		
+		int x = coordStack[0].x;
+		int y = coordStack[0].y;
+		coordStack.RemoveAt(0);
+		
+		maze[y, x, 0] = 'f';
+		maze[y, x, 1] = 'v';
+		
+		int chance = 2, chanceIncrement = 4;
+		bool added = false;
+		
+		if((y - 1) > 0)
+			if(maze[y-1, x, 1] != 'v')
+				if(Random.Range(0, chance) == 0)
+				{
+					chance *= chanceIncrement;
+					coordStack.Insert(0, new Coordinate(x, y-1));
+					added = true;
+					mazeSize++;
+				}
+				else
+				{
+					maze[y-1, x, 0] = 'w';
+					maze[y-1, x, 1] = 'v';
+					chance /= chanceIncrement;
+				}
+				
+		if((x + 1) < xsize)
+			if(maze[y, x+1, 1] != 'v')
+				if(Random.Range(0, chance) == 0)
+			{
+				chance *= chanceIncrement;
+				coordStack.Insert(0, new Coordinate(x+1, y));
+				added = true;					
+				mazeSize++;
+			}
+			else
+			{
+				maze[y, x+1, 0] = 'w';
+				maze[y, x+1, 1] = 'v';
+				chance /= chanceIncrement;
+				
+			}			
+		
+		if((y + 1) < ysize)
+		   if(maze[y+1, x, 1] != 'v')
+			   if(Random.Range(0, chance) == 0)
+			   {
+					chance *= chanceIncrement;
+					coordStack.Insert(0, new Coordinate(x, y+1));
+					added = true;
+					mazeSize++;
+				}
+				else
+				{
+					maze[y+1, x, 0] = 'w';
+					maze[y+1, x, 1] = 'v';
+					chance /= chanceIncrement;
+				}				
+					
+		
+		if((x -1) > 0)
+			if(maze[y, x-1, 1] != 'v')
+				if(Random.Range(0, chance) == 0)
+				{
+					chance *= chanceIncrement;
+					coordStack.Insert(0, new Coordinate(x-1, y));
+					added = true;					
+					mazeSize++;
+				}
+				else
+				{
+					maze[y, x-1, 0] = 'w';
+					maze[y, x-1, 1] = 'v';
+					chance /= chanceIncrement;
+					
+				}
+				
+		
+		fillInMaze(coordStack);
+	}
+	
+	//Probably a bad implementation, froze unity last time I tried
+	void fillInMaze2(List<Coordinate> coordStack)
 	{
 		if(coordStack.Count < 1)
 			return;
@@ -90,70 +190,55 @@ public class create_map : MonoBehaviour {
 		maze[y, x, 0] = 'f';
 		maze[y, x, 1] = 'v';
 				
-		int chance = 2, chanceIncrement = 7;
-		bool added = false;
+		int chance = 2, chanceIncrement = 4;
+		//Added and counter provide a way to break out of the while loop
+		int added = 0, newX, newY, counter = 0;
 		
-		if((y - 1) > 0)
-			if(maze[y-1, x, 1] != 'v')
-				if(Random.Range(0, chance) == 0)
-				{
-					chance *= chanceIncrement;
-					coordStack.Insert(0, new Coordinate(x, y-1));
-					added = true;
-				}
-				else
-				{
-					maze[y-1, x, 0] = 'w';
-					maze[y-1, x, 1] = 'v';
-					chance /= chanceIncrement;
-				}
+		
+		
+		while(added < 2 && counter < 4)
+		{
+			int direction = Random.Range(0, 4);
+			newY = y;
+			newX = x;			
+			//Up direction
+			if(direction == 0)
+				newY = y - 1;
+			//Down direction
+			else if(direction == 1)
+				newY = y + 1;
+			//Left direction
+			else if(direction == 2)
+				newX = x - 1;
+			//Right direction
+			else
+				newX = x + 1;
 				
-		if((y + 1) < ysize)
-		   if(maze[y+1, x, 1] != 'v')
-			   if(Random.Range(0, chance) == 0)
-			   {
-					chance *= chanceIncrement;
-					coordStack.Insert(0, new Coordinate(x, y+1));
-					added = true;
-				}
-				else
-				{
-					maze[y+1, x, 0] = 'w';
-					maze[y+1, x, 1] = 'v';
-					chance /= chanceIncrement;
-				}				
-					
-		if((x + 1) < xsize)
-			if(maze[y, x+1, 1] != 'v')
-				if(Random.Range(0, chance) == 0)
-				{
-					chance *= chanceIncrement;
-					coordStack.Insert(0, new Coordinate(x+1, y));
-					added = true;					
-				}
-				else
-				{
-					maze[y, x+1, 0] = 'w';
-					maze[y, x+1, 1] = 'v';
-					chance /= chanceIncrement;
-					
-				}			
-		if((x -1) > 0)
-			if(maze[y, x-1, 1] != 'v')
-				if(Random.Range(0, chance) == 0)
-				{
-					chance *= chanceIncrement;
-					coordStack.Insert(0, new Coordinate(x-1, y));
-					added = true;					
-				}
-				else
-				{
-					maze[y, x-1, 0] = 'w';
-					maze[y, x-1, 1] = 'v';
-					chance /= chanceIncrement;
-					
-				}
-		fillInMaze(coordStack);
+			if(newX < 0 || newX >= xsize || newY < 0 || newY > ysize)
+				continue;
+			
+			print (newX + " " + newY);
+			
+			if(maze[newY, newX, 1] == 'v')
+				continue;
+				
+			if(Random.Range(0, chance) == 0)
+			{
+				chance *= chanceIncrement;
+				coordStack.Insert(0, new Coordinate(newX, newY));
+				added++;
+			}
+			else
+			{
+				maze[newY, newX, 0] = 'w';
+				maze[newY, newX, 1] = 'v';
+				chance /= chanceIncrement;
+			}
+			
+			counter++;
+		}
+		
+		fillInMaze2(coordStack);
 	}
 	
 	void createRandomMap()
@@ -169,6 +254,11 @@ public class create_map : MonoBehaviour {
 
 	void createBorder()
 	{
+		upperBound = (ysize * increment / 2) + increment;
+		rightBound = (xsize * increment / 2);
+		lowerBound = (upperBound * -1) + increment;
+		leftBound = (rightBound * -1) - increment;
+	
 		Instantiate (top_left_corner, new Vector3 (leftBound, upperBound, 0.0f), Quaternion.identity);
 		Instantiate (top_right_corner, new Vector3 (rightBound, upperBound, 0.0f), Quaternion.identity);
 		Instantiate (bottom_left_corner, new Vector3 (leftBound, lowerBound, 0.0f), Quaternion.identity);
@@ -176,12 +266,12 @@ public class create_map : MonoBehaviour {
 		//Instantiate (bottom_right_corner, new Vector3 (leftBound + 4, upperBound - 4, -1f), Quaternion.identity);
 
 		//Creating the border
-		for (float x = leftBound + increment; x < 15.64f; x += increment) {
+		for (float x = leftBound + increment; x < rightBound; x += increment) {
 			Instantiate (wall_horizontal, new Vector3 (x, upperBound, 0.0f), Quaternion.identity);
 			Instantiate (wall_horizontal, new Vector3 (x, lowerBound, 0.0f), Quaternion.identity);
 		}
 
-		for (float y = upperBound; y > -7.64f; y -= increment) {
+		for (float y = upperBound; y > lowerBound; y -= increment) {
 			Instantiate (wall_vertical, new Vector3 (leftBound, y, 0.0f), Quaternion.identity);
 			Instantiate (wall_vertical, new Vector3 (rightBound, y, 0.0f), Quaternion.identity);
 		}
