3a952ae5944d23c5236732a0141c42255c36387c
FLESHRAM <FLESHRAM@github.com>
2015-04-27 18:59:14 -0400

    testing

---
+++ Horror Game/Assets/Scripts/Director.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Director : MonoBehaviour {
+
+	GameObject
+
+	// Use this for initialization
+	void Awake () {
+		DontDestroyOnLoad (gameObject);
+		if(Application.loadedLevel == 1)
+		{
+
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
